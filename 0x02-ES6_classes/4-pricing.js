a963536c76f15441658db8c48dd79d86a4229729
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-06-29 23:11:38 +0100

    feat(0x02): define class `Pricing`

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Pricing amount must be a number');
+    }
+
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Pricing currency must be a Currency');
+    }
+
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError(
+        'Pricing amount and conversion rates must be numbers',
+      );
+    }
+
+    return amount * conversionRate;
+  }
+}
